5169222e945ffae7ead85a89e1c40f09b4a6b470
Dhaval <dhavalpatel76126@gmail.com>
2022-09-18 13:06:51 +0530

    initial commit

---
+++ src/components/employee/edit.component.js
@@ -0,0 +1,178 @@
+import React, { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+export default function EditUser() {
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [image, setImage] = useState("");
+  const [validationError, setValidationError] = useState({});
+
+  useEffect(() => {
+    fetchEmployee();
+  }, []);
+
+  const fetchEmployee = async () => {
+    await axios
+      .get(`http://localhost:8000/api/employee/${id}`)
+      .then(({ data }) => {
+        const { name, email, age, image } = data.employee;
+        setName(name);
+        setEmail(email);
+        setAge(age);
+        setImage(image);
+      })
+      .catch(({ response: { data } }) => {
+        Swal.fire({
+          text: data.message,
+          icon: "error",
+        });
+      });
+  };
+//   const changeHandler = (event) => {
+//     setImage(event.target.files[0]);
+//   };
+
+  const updateEmployee = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("_method", "PATCH");
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("age", age);
+    if (image !== null) {
+      formData.append("image", image);
+    }
+
+    await axios
+      .post(`http://localhost:8000/api/employee/${id}`, formData)
+      .then(({ data }) => {
+        Swal.fire({
+          icon: "success",
+          text: data.message,
+        });
+        navigate("/");
+      })
+      .catch(({ response }) => {
+        if (response.status === 422) {
+          setValidationError(response.data.errors);
+        } else {
+          Swal.fire({
+            text: response.data.message,
+            icon: "error",
+          });
+        }
+      });
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-12 col-sm-12 col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h4 className="card-name">Update Employee</h4>
+              <hr />
+              <div className="form-wrapper">
+                {Object.keys(validationError).length > 0 && (
+                  <div className="row">
+                    <div className="col-12">
+                      <div className="alert alert-danger">
+                        <ul className="mb-0">
+                          {Object.entries(validationError).map(
+                            ([key, value]) => (
+                              <li key={key}>{value}</li>
+                            )
+                          )}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                )}
+                <Form onSubmit={updateEmployee}>
+                  <Row className="my-3">
+                    <Col>
+                      <Form.Group controlId="Name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                          type="text"
+                          value={name}
+                          onChange={(event) => {
+                            setName(event.target.value);
+                          }}
+                        />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <Row className="my-3">
+                    <Col>
+                      <Form.Group controlId="Email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                          type="email"
+                          value={email}
+                          onChange={(event) => {
+                            setEmail(event.target.value);
+                          }}
+                        />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <Row className="my-3">
+                    <Col>
+                      <Form.Group controlId="Age">
+                        <Form.Label>Age</Form.Label>
+                        <Form.Control
+                          type="number"
+                          value={age}
+                          onChange={(event) => {
+                            setAge(event.target.value);
+                          }}
+                        />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Form.Group controlId="Image" className="mb-3">
+                        <Form.Label>Image</Form.Label>
+                        <Form.Control type="file"/>
+                      </Form.Group>
+                    </Col>
+                    <img
+                      style={{
+                        width: 100,
+                      }}
+                      src={`http://localhost:8000/storage/employee/image/${image}`}
+                      alt="employee"
+                    />
+                  </Row>
+                  <Button
+                    variant="primary"
+                    className="mt-2"
+                    size="lg"
+                    block="block"
+                    type="submit"
+                  >
+                    Update
+                  </Button>
+                </Form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/employee/list.component.js
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+export default function List() {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+
+  const fetchEmployees = async () => {
+    await axios.get(`http://localhost:8000/api/employee`).then(({ data }) => {
+      setEmployees(data);
+    });
+  };
+
+  const deleteEmployee = async (id) => {
+    const isConfirm = await Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      return result.isConfirmed;
+    });
+
+    if (!isConfirm) {
+      return;
+    }
+
+    await axios
+      .delete(`http://localhost:8000/api/employee/${id}`)
+      .then(({ data }) => {
+        Swal.fire({
+          icon: "success",
+          text: data.message,
+        });
+        fetchEmployees();
+      })
+      .catch(({ response: { data } }) => {
+        Swal.fire({
+          text: data.message,
+          icon: "error",
+        });
+      });
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <Link
+            className="btn btn-primary mb-2 float-end"
+            to={"/employee/create"}
+          >
+            Create Employee
+          </Link>
+        </div>
+        <div className="col-12">
+          <div className="card card-body">
+            <div className="table-responsive">
+              <table className="table table-bordered mb-0 text-center">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Age</th>
+                    <th>Image</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {employees.length > 0 &&
+                    employees.map((row, key) => (
+                      <tr key={key}>
+                        <td>{row.name}</td>
+                        <td>{row.email}</td>
+                        <td>{row.age}</td>
+                        <td>
+                          <img
+                            width="50px"
+                            src={`http://localhost:8000/storage/employee/image/${row.image}`}
+                            alt={key}
+                          />
+                        </td>
+                        <td>
+                          <Link
+                            to={`/employee/edit/${row.id}`}
+                            className="btn btn-success me-2"
+                          >
+                            Edit
+                          </Link>
+                          <Button
+                            variant="danger"
+                            onClick={() => deleteEmployee(row.id)}
+                          >
+                            Delete
+                          </Button>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
